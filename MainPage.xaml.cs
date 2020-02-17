a992758a44c33b9b9c0b663b1a0556e11276d274
jcvdmerwe03@gmail.com <jcvdmerwe03@gmail.com>
2020-02-17 13:04:12 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Tip_Calculator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnBillChanged(object sender, TextChangedEventArgs e)
+        {
+            double bill;
+            if (!double.TryParse(billInput.Text, out bill))
+                return;
+            double tip = bill * 0.15;
+            tipOutput.Text = string.Format("{0:C}", tip);
+        }
+    }
+}
